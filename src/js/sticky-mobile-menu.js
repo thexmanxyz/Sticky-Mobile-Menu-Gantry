962bd6b9c2105f6f856dc7b8002cb822065999f1
thex <andreas.kar@gmx.at>
2018-05-24 02:22:26 +0200

    touchstart included, slide-out fixed, orphaned classes removed

--- src/js/sticky-mobile-menu.js
+++ src/js/sticky-mobile-menu.js
@@ -13,52 +13,59 @@
 ******************************************************/
 
 jQuery(document).ready(function () {
-	jQuery(".g-offcanvas-toggle").click(setMobileMenuOpen);	
+    jQuery(".g-offcanvas-toggle").on('touchstart click', setMobileMenuOpen);
 });
 
 /*jQuery(window).load(function () {
-	jQuery(".g-offcanvas-toggle").click(setMobileMenuOpen);	
+    jQuery(".g-offcanvas-toggle").on('touchstart click', setMobileMenuOpen);
 });*/
 
 function setMobileMenuOpen(){
-	var topMSel = ".g-toplevel";
-	var subMSel = ".g-sublevel";
-	var ddCSel = ".g-dropdown-column";
-	var ddSel = ".g-dropdown";
-	var mItmSel = ".g-menu-item";
-	var aMItmSel = ".g-menu-item.active";
-	
-	var slideCls = "g-slide-out";
-	var selCls = "g-selected";
-	var inacCls = "g-inactive";
-	var acCls = "g-active";
-	
-	var lowest = 0;
-	var $deepestItem;
-	
-	var $activeItem = jQuery(aMItmSel);
+    var slideCls = "g-slide-out";
+    var selCls = "g-selected";
+    var inacCls = "g-inactive";
+    var acCls = "g-active";
 
-	$activeItem.each(function() {
-		var depth = jQuery(this).parents().length;
-		if (depth > lowest) {
-			$deepestItem = jQuery(this);
-			lowest = depth;
-		}
-	});
+    var slideClsSel = "." + slideCls;
+    var selClsSel = "." + selCls;
+    var acClsSel = "." + acCls;
 
-	if ($deepestItem.parents(subMSel).length) {
-		var $nextDDC = $deepestItem.parents(ddCSel)
-		var $topLevel = $nextDDC.parents(topMSel);
-		var $subLevel = $nextDDC.parents(subMSel);
-		var $dropDown = $nextDDC.parents(ddSel);
-		var $menuItem = $nextDDC.parents(mItmSel);
-		
-		if($subLevel.length > 0)
-			$subLevel.addClass(slideCls);
-		
-		$topLevel.addClass(slideCls);
-		$dropDown.removeClass(inacCls);
-		$dropDown.addClass(acCls);
-		$menuItem.addClass(selCls);
-	}
+    var topMSel = ".g-toplevel";
+    var subMSel = ".g-sublevel";
+    var ddCSel = ".g-dropdown-column";
+    var ddSel = ".g-dropdown";
+    
+    var mItmSel = ".g-menu-item";
+    var aMItmSel = mItmSel + ".active";
+
+    var lowest = 0;
+    var $deepestItem;
+    
+    var $activeItem = jQuery(aMItmSel);
+
+    $activeItem.each(function() {
+        var depth = jQuery(this).parents().length;
+        if (depth > lowest) {
+            $deepestItem = jQuery(this);
+            lowest = depth;
+        }
+    });
+
+    var $nextDDC = $deepestItem.parents(ddCSel)
+    var $topLevel = $nextDDC.parents(topMSel);
+    var $subLevel = $nextDDC.parents(subMSel);
+    var $dropDown = $nextDDC.parents(ddSel);
+    var $menuItem = $nextDDC.parents(mItmSel);
+    
+    jQuery(slideClsSel).removeClass(slideCls);
+    jQuery(selClsSel).removeClass(selCls);
+    jQuery(acClsSel).removeClass(acCls);
+
+    if($subLevel.length > 0)
+        $subLevel.addClass(slideCls);
+
+    $topLevel.addClass(slideCls);
+    $dropDown.removeClass(inacCls);
+    $dropDown.addClass(acCls);
+    $menuItem.addClass(selCls);
 }
